f0d65289d05cd2ceb572d9900d7565a2705fc824
sinly2 <15380794704@163.com>
2017-05-26 19:13:35 +0800

    测试

--- app/manage.py
+++ app/manage.py
@@ -7,6 +7,7 @@ Created on 2017年3月3日
 import sys
 sys.path.append('/root/project/TestRequests')
 from app import create_app
+#test
 app = create_app()
 if __name__ == '__main__':
     app.run(debug=True,port=80)
